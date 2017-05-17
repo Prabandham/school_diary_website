08ce1afd1b3d9e1ad1717fa883cd559e4f6149f2
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2017-05-17 16:15:41 +0530

    removing page changeset

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule SchoolDiary.PageController do
   use SchoolDiary.Web, :controller
 
   def index(conn, _params) do
-    contact_changeset = Contact.changeset(%Contact{})
-    render conn, "index.html", contact_changeset: contact_changeset
+  #    contact_changeset = Contact.changeset(%Contact{})
+    render conn, "index.html"#, contact_changeset: contact_changeset
   end
 end
